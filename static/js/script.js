8e4c0acd3b71f266efedc45e38be428add251a1f
Javier <123848754+eljega@users.noreply.github.com>
2023-10-01 09:02:09 -0400

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,30 @@
+// script.js
+
+const chatBox = document.getElementById('chat-box');
+const userInput = document.getElementById('user-input');
+const sendButton = document.getElementById('send-button');
+
+sendButton.addEventListener('click', () => {
+    const userMessage = userInput.value;
+    userInput.value = '';
+    appendMessage('user', userMessage);
+
+    fetch('/chatbot', {
+        method: 'POST',
+        body: new URLSearchParams({ user_message: userMessage }),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    })
+    .then(response => response.text())
+    .then(botReply => {
+        appendMessage('bot', botReply);
+    });
+});
+
+function appendMessage(sender, message) {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add(sender);
+    messageElement.innerText = message;
+    chatBox.appendChild(messageElement);
+}
